7815a93c1e38dec22d56a5ed6e215c9ffd02298c
Sarobidy <hei.sarbidy.8@gmail.com>
2026-08-24 15:03:01 +0300

    feat(security): add middleware

---
+++ src/security/roleMiddleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+export const roleMiddleware = (role: "ADMIN" | "STUDENT") => {
+    return (
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ) => {
+        if (!req.user) {
+            return res.status(401).json({message: "Authentication required"});
+        }
+
+        if (req.user.role !== role) {
+            return res.status(403).json({message: "Access denied" });
+        }
+
+        next();
+    };
+}
