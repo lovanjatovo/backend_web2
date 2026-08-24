69de162356ce624f5e50bdac77874b414eee9cda
Sarobidy <hei.sarbidy.8@gmail.com>
2026-08-24 12:33:47 +0300

    feat: add jwt

---
+++ src/security/jwt.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+function getJwtSecret(): string {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) { throw new Error("JWT_SECRET is not defined");}
+    return secret;
+}
+
+export function generateToken(
+    userId: number,
+    role: "ADMIN" | "STUDENT"
+) {
+    return jwt.sign(
+        { userId, role },
+        getJwtSecret(),
+        { expiresIn: "1h" }
+    );
+}
