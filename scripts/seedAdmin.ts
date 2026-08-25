8677681734eeb6e7458ecdc8e42b0b1a7467ca00
Allan <hei.allan.30@gmail.com>
2026-08-25 16:45:07 +0300

    feat(scripts): add seedAdmin

---
+++ scripts/seedAdmin.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import { pool } from "../src/Configuration/db";
+
+async function seedAdmin() {
+    try {
+        const email = "admin@exam.com";
+        const password = "Admin123";
+
+        const existingAdmin = await pool.query(
+            `SELECT id FROM users
+             WHERE email = $1`,
+            [email]
+        );
+
+        if (existingAdmin.rows.length > 0) {
+            console.log("The admin panel already exists.");
+            return;
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        await pool.query(
+            `INSERT INTO users
+                (email, password_hash, role, is_active)
+             VALUES
+                ($1, $2, 'ADMIN', true)`,
+            [email, passwordHash]
+        );
+
+        console.log("Admin created successfully.");
+
+    } catch (error) {
+        console.error("Error occurred while creating the admin :", error);
+
+    } finally {
+        await pool.end();
+    }
+}
+
+seedAdmin();
